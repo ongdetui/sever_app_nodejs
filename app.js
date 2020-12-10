e13c5c147e279024daad03d82abc9dbb9cdfc795
ongdetui <ducphamvan0711@gmail.com>
2020-12-11 00:22:26 +0700

    make it better

--- app.js
+++ app.js
@@ -8,6 +8,24 @@ var cors = require('cors');
 
 var app = express.createServer(express.logger()),
 io = require('socket.io').listen(app);
+
+app.configure(function() {
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function() {
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function() {
+  app.use(express.errorHandler());
+});
+
 io.configure(function () { 
   io.set("transports", ["xhr-polling"]); 
   io.set("polling duration", 10); 
