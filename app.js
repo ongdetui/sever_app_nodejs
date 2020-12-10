e958ec02bea236f58e751329c4324ce780774c7a
ongdetui <ducphamvan0711@gmail.com>
2020-12-10 23:03:12 +0700

    make it better

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require('cors')
+var cors = require('cors');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,9 +23,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors());
+// app.use(cors());
 app.use('/', indexRouter);
-app.use('/product', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
