17309a49f957e45507973f645f69a69b30060a33
ongdetui <ducphamvan0711@gmail.com>
2020-12-10 22:57:54 +0700

    make it better

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-// app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000);
 
 
 // view engine setup
